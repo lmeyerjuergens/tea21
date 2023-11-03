561f0bff0d7098e27f58d479cf44b70a953f5c40
lmeyerjuergens <149794503+lmeyerjuergens@users.noreply.github.com>
2023-11-03 15:22:35 +0100

    Ergänzung

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -28,29 +28,13 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Value of variable bss {} address of variable bss {}\n",
-    bss,
-    fmt::ptr(&bss));
-
-    fmt::print("Value of variable data {} address of variable data {}\n",
-    data,
-    fmt::ptr(&data));
-
-    fmt::print("Value of variable rodata {} address of variable rodata {}\n",
-    rodata,
-    fmt::ptr(&rodata));
-
-    fmt::print("Value of variable bss {} address of variable bss {}\n",
-    bss2,
-    fmt::ptr(&bss2));
-
-    fmt::print("Value of variable data {} address of variable data {}\n",
-    data2,
-    fmt::ptr(&data2));
-
-    fmt::print("Value of variable rodata {} address of variable rodata {}\n",
-    rodata2,
-    fmt::ptr(&rodata2));
+    
+    fmt::print("Value of variable bss {} address of variable bss {}\n", bss, fmt::ptr(&bss));
+    fmt::print("Value of variable data {} address of variable data {}\n", data, fmt::ptr(&data));
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n", rodata, fmt::ptr(&rodata));
+    fmt::print("Value of variable bss {} address of variable bss {}\n", bss2, fmt::ptr(&bss2));
+    fmt::print("Value of variable data {} address of variable data {}\n", data2, fmt::ptr(&data2));
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n", rodata2, fmt::ptr(&rodata2));
 
     return 0; /* exit gracefully*/
 }
