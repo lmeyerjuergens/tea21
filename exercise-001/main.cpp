3fd50b636dcfaded5a5129973c1530f3076b21cc
lmeyerjuergens <149794503+lmeyerjuergens@users.noreply.github.com>
2023-11-03 15:30:41 +0100

    corrected

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -33,11 +33,11 @@ auto main(int argc, char **argv) -> int
     fmt::print("Value of variable bss {} address of variable bss {}\n", bss, fmt::ptr(&bss));
     fmt::print("Value of variable data {} address of variable data {}\n", data, fmt::ptr(&data));
     fmt::print("Value of variable rodata {} address of variable rodata {}\n", rodata, fmt::ptr(&rodata));
-    fmt::print("Value of variable bss {} address of variable bss {}\n", bss2, fmt::ptr(&bss2));
-    fmt::print("Value of variable data {} address of variable data {}\n", data2, fmt::ptr(&data2));
-    fmt::print("Value of variable rodata {} address of variable rodata {}\n", rodata2, fmt::ptr(&rodata2));
-    fmt::print("Value of variable rodata {} address of variable rodata {}\n", stack, fmt::ptr(&stack));
-    fmt::print("Value of variable rodata {} address of variable rodata {}\n", stack2, fmt::ptr(&stack2));
+    fmt::print("Value of variable stack {} address of variable rodata {}\n", stack, fmt::ptr(&stack));
+    fmt::print("Value of variable bss2 {} address of variable bss2 {}\n", bss2, fmt::ptr(&bss2));
+    fmt::print("Value of variable data2 {} address of variable data2 {}\n", data2, fmt::ptr(&data2));
+    fmt::print("Value of variable rodata2 {} address of variable rodata2 {}\n", rodata2, fmt::ptr(&rodata2));
+    fmt::print("Value of variable stack2 {} address of variable rodata {}\n", stack2, fmt::ptr(&stack2));
 
     return 0; /* exit gracefully*/
 }
