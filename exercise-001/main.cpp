6f7e5f0f4649764d89fdd01549057b3a0d7057da
lmeyerjuergens <149794503+lmeyerjuergens@users.noreply.github.com>
2023-11-03 15:31:16 +0100

    corrected again

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -37,7 +37,7 @@ auto main(int argc, char **argv) -> int
     fmt::print("Value of variable bss2 {} address of variable bss2 {}\n", bss2, fmt::ptr(&bss2));
     fmt::print("Value of variable data2 {} address of variable data2 {}\n", data2, fmt::ptr(&data2));
     fmt::print("Value of variable rodata2 {} address of variable rodata2 {}\n", rodata2, fmt::ptr(&rodata2));
-    fmt::print("Value of variable stack2 {} address of variable rodata {}\n", stack2, fmt::ptr(&stack2));
+    fmt::print("Value of variable stack2 {} address of variable stack2 {}\n", stack2, fmt::ptr(&stack2));
 
     return 0; /* exit gracefully*/
 }
