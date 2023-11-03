2297543d54a13eecb32d260b6cbf662c40aeeeb3
lmeyerjuergens <149794503+lmeyerjuergens@users.noreply.github.com>
2023-11-03 15:08:43 +0100

    changed main.cpp

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -8,7 +8,7 @@
 #include "config.h"
 
 // .bss segment
-int foo;
+int bss;
 
 // .data segment
 int data = 4711;
@@ -24,15 +24,15 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Value of variable foo {} address of variable foo {}",
-    foo,
-    fmt::ptr(&foo));
+    fmt::print("Value of variable bss {} address of variable bss {}\n",
+    bss,
+    fmt::ptr(&bss));
 
-    fmt::print("Value of variable foo {} address of variable foo {}",
+    fmt::print("Value of variable data {} address of variable data {}\n",
     data,
     fmt::ptr(&data));
 
-    fmt::print("Value of variable foo {} address of variable foo {}",
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n",
     rodata,
     fmt::ptr(&rodata));
     return 0; /* exit gracefully*/
