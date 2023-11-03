6c3ad0c3a36e80aac7373be4ea59cab59b777653
lmeyerjuergens <149794503+lmeyerjuergens@users.noreply.github.com>
2023-11-03 15:29:04 +0100

    added stack variable

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -22,19 +22,22 @@ const int rodata2 = 24;
 
 auto main(int argc, char **argv) -> int
 {
-   
+   int stack;
+   int stack2 = 22;
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    
+
     fmt::print("Value of variable bss {} address of variable bss {}\n", bss, fmt::ptr(&bss));
     fmt::print("Value of variable data {} address of variable data {}\n", data, fmt::ptr(&data));
     fmt::print("Value of variable rodata {} address of variable rodata {}\n", rodata, fmt::ptr(&rodata));
     fmt::print("Value of variable bss {} address of variable bss {}\n", bss2, fmt::ptr(&bss2));
     fmt::print("Value of variable data {} address of variable data {}\n", data2, fmt::ptr(&data2));
     fmt::print("Value of variable rodata {} address of variable rodata {}\n", rodata2, fmt::ptr(&rodata2));
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n", stack, fmt::ptr(&stack));
+    fmt::print("Value of variable rodata {} address of variable rodata {}\n", stack2, fmt::ptr(&stack2));
 
     return 0; /* exit gracefully*/
 }
